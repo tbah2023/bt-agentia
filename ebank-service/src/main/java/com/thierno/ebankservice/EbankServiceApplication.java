5632de0cc9b2dbee33b44c4c5cf69f49d802cd8f
tbah2023 <btcmoussa@gmail.com>
2026-04-06 11:32:38 +0200

    feat: ajout du module ebank-service

---
+++ ebank-service/src/main/java/com/thierno/ebankservice/EbankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thierno.ebankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankServiceApplication.class, args);
+    }
+
+}
